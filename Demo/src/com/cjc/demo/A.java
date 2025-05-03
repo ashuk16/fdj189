cc1826b4ac451e7bc6dfd030eae8f49fe906ffd5
kalea <kalea@Ashutosh.lan>
2025-05-03 14:07:16 +0530

    added by ashu

--- Demo/src/com/cjc/demo/A.java
+++ Demo/src/com/cjc/demo/A.java
@@ -2,10 +2,19 @@ package com.cjc.demo;
 
 public class A {
 	
+	
+	public void m1()
+	{
+		System.out.println("This is m1 method");
+	}
 	public static void main(String args[])
 	{
 		
 		System.out.println("This is first method");
-	}
+	
+	
+	A obj=new A();
+	obj.m1();
 
+}
 }
